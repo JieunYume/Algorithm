851c9cd7c1fafc56656e24e6401c117abf982502
이지은 <89847820+JieunYume@users.noreply.github.com>
2024-03-09 00:28:36 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 1.39 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -1,21 +1,14 @@
-import java.util.*;
 class Solution {
-    public String solution(String s) {
+  public String solution(String s) {
         String answer = "";
-        
-        String[] words = s.split(" ");
-        for(int i=0; i<words.length; i++){
-            if(words[i].length() != 0){
-                answer = answer + words[i].substring(0, 1).toUpperCase() 
-                    + words[i].substring(1).toLowerCase() + " ";
-            }else{
-                answer += " ";
-            }
+        String[] sp = s.toLowerCase().split("");
+        boolean flag = true;
+
+        for(String ss : sp) {
+            answer += flag ? ss.toUpperCase() : ss;
+            flag = ss.equals(" ") ? true : false;
         }
-        
-        if(s.substring(s.length()-1, s.length()).equals(" ")){
-            return answer;
-        }
-        return answer.substring(0, answer.length()-1);
-    }
+
+        return answer;
+  }
 }
--- 프로그래머스/2/12951. JadenCase 문자열 만들기/README.md
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 2] JadenCase 문자열 만들기 - 12951 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12951) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12951#) 
 
 ### 성능 요약
 
-메모리: 80.6 MB, 시간: 13.30 ms
+메모리: 73.1 MB, 시간: 1.39 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 09일 00:23:42
+2024년 03월 09일 00:28:36
 
 ### 문제 설명
 
