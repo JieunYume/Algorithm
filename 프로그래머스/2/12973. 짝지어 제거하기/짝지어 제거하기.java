894482e8005e9001aeb1368058df0409afeeb873
이지은 <89847820+JieunYume@users.noreply.github.com>
2024-03-22 01:17:23 +0900

    [level 2] Title: 짝지어 제거하기, Time: 48.67 ms, Memory: 59.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution
+{
+    public int solution(String s) {
+        Stack<Character> stack = new Stack<>();
+        
+        for(int i=0; i<s.length(); i++){
+            if(stack.isEmpty()){
+                stack.push(s.charAt(i));
+            }
+            else{
+                if(stack.peek() == s.charAt(i)){
+                    stack.pop();
+                } else{
+                    stack.push(s.charAt(i));
+                }
+            }
+        }
+        if(stack.isEmpty()){
+            return 1;
+        }else{
+            return 0;
+        }
+    }
+}
