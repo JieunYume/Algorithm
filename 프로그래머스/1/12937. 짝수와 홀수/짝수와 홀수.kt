2576580a50b2131456454762559afee481dc4b93
이지은 <89847820+JieunYume@users.noreply.github.com>
2024-04-03 17:11:19 +0900

    [level 1] Title: 짝수와 홀수, Time: 0.02 ms, Memory: 61.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.kt
@@ -0,0 +1,9 @@
+class Solution {
+    fun solution(num: Int): String {
+        var answer = ""
+        
+        if(num%2==0) answer = "Even" else answer = "Odd"
+        
+        return answer
+    }
+}
